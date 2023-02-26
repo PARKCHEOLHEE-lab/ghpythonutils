5277560b6aa3c3d7ffd3035378353cc914cfdf0a
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-02-26 17:34:12 +0900

    kmeans

--- utils/utils.py
+++ utils/utils.py
@@ -10,11 +10,11 @@ class Enum:
     @staticmethod
     def enum(*names):
         """Enumerated type creation helper
-        Args:
-            names: str
-        Return:
-            Enum
+
+        Returns:
+            Enum: custom enumerated type
         """
+        
         enums = {}
         for ni, name in enumerate(names):
             enums[name] = ni
@@ -26,17 +26,22 @@ class LineHelper:
     @staticmethod
     def get_line_2d_angle(linestring, is_radians=True):
         """Calculate angle between two points
+
         Args:
-            linestring: Union[
-                Rhino.Geometry.Curve, 
-                Rhino.Geometry.PolylineCurve, 
-                Rhino.Geometry.LineCurve,
-                Rhino.Geometry.PolyCurve,
-            ]
-        Return:
-            float
+            linestring (
+                Union[
+                    Rhino.Geometry.Curve, 
+                    Rhino.Geometry.PolylineCurve, 
+                    Rhino.Geometry.LineCurve,
+                    Rhino.Geometry.PolyCurve,
+                ]
+            ): Segment with only two points
+            is_radians (bool, optional): If False return to degree. Defaults to True.
+
+        Returns:
+            float: Angle of given linestring
         """
-        
+                
         x1, y1, _ = linestring.PointAtStart
         x2, y2, _ = linestring.PointAtEnd
         
@@ -51,12 +56,14 @@ class NumericHelper:
     @staticmethod
     def is_close(n1, n2, tolerance=ConstsCollection.TOLERANCE):
         """Check if two numbers are within margin of error
+
         Args:
-            n1: float
-            n2: float
-            tolerance: float
-        Return:
-            bool
+            n1 (float): Number to compare 
+            n2 (float): Number to compare
+            tolerance (float, optional): Permissible range. Defaults to ConstsCollection.TOLERANCE.
+
+        Returns:
+            bool: Compare result about whether two numbers are equal
         """
         
         return abs(n1 - n2) <= tolerance
@@ -66,10 +73,12 @@ class SurfaceHelper:
     @staticmethod
     def surface_reparameterize(srf):
         """Change domain to 0 ~ 1 given surface
-            Args:
-                srf: Rhino.Geometry.Surface
-            Retrun:
-                Rhino.Geometry.Surface
+
+        Args:
+            srf (Rhino.Geometry.Surface): Surface to reparameterize
+
+        Returns:
+            Rhino.Geometry.Surface: Reparameterized surface
         """
         
         interval = rg.Interval(0, 1)
@@ -77,3 +86,20 @@ class SurfaceHelper:
         srf.SetDomain(1, interval)
         
         return srf
+
+
+class PointHelper:
+    @staticmethod
+    def get_points_cloud_centroid(points):
+        """Find a centroid of given points.
+
+        Args:
+            points (List[Rhino.Geometry.Point3d]): Points cloud
+
+        Returns:
+            Rhino.Geometry.Point3d: Centroid of points cloud 
+        """
+        
+        return rg.Point3d(
+            *[sum(coord_list) / len(points) for coord_list in zip(*points)]
+        )
