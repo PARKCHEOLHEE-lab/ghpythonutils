be976f3203ef476444a034b7a3163f05e05c4d66
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-04-11 21:31:38 +0900

    quadtree wip

--- utils/quadtree/quadtree.py
+++ utils/quadtree/quadtree.py
@@ -1,67 +1,123 @@
-﻿import Rhino.Geometry as rg
+﻿from ghpythonutils.utils.utils import LineHelper, ConstsCollection
 
-from ghpythonutils.utils.utils import ConstsCollection, LineHelper
+import Rhino.Geometry as rg
 
 
 class Quadtree:
     COUNTER = 1
-
-    def __init__(self, min_size, bounding_box):
+    
+    def __init__(self, bounding_box, min_size=2):
         self.bounding_box = bounding_box
         self.min_size = min_size
-
+        
         self.crvs = []
         self.children = None
         self.children_geometries = None
-
+        
+        self._post_init()
+        
+    def _post_init(self):
+        exploded_bounding_box = self.bounding_box.DuplicateSegments()
+        
+        self.width, self.height, _, _ = [
+            seg.GetLength() for seg in exploded_bounding_box
+        ]
+        
+        is_plane_created, self.plane = exploded_bounding_box[0].FrameAt(
+            ConstsCollection.HALF
+        )
+        
+        centroid = rg.AreaMassProperties.Compute(self.bounding_box).Centroid
+        
+        self.plane.Origin = centroid
+        
     def _subdivide(self):
-        pass
+        
+        ne_bb = rg.Rectangle3d(self.plane, self.width / 2, self.height / 2)
+        ne_bb = ne_bb.ToNurbsCurve()
+        
+        nw_bb = rg.Rectangle3d(self.plane, -self.width / 2, self.height / 2)
+        nw_bb = nw_bb.ToNurbsCurve()
+        
+        se_bb = rg.Rectangle3d(self.plane, self.width / 2, -self.height / 2)
+        se_bb = se_bb.ToNurbsCurve()
+        
+        sw_bb = rg.Rectangle3d(self.plane, -self.width / 2, -self.height / 2)
+        sw_bb = sw_bb.ToNurbsCurve()
+        
+        ne = Quadtree(ne_bb)
+        nw = Quadtree(nw_bb)
+        se = Quadtree(se_bb)
+        sw = Quadtree(sw_bb)
 
-    def _is_intersects(self, crv):
-        p1 = crv.PointAtStart
-        p2 = crv.PointAtEnd
+        self.children = [ne, nw, se, sw]
+        self.children_geometries = [ne_bb, nw_bb, se_bb, sw_bb]
 
-        for p in (p1, p2):
-            condition = self.bounding_box.Contains(p)
-            is_intersects = condition in (
-                rg.PointContainment.Inside,
-                rg.PointContainment.Coincident,
-            )
-
-            if is_intersects:
-                return True
-
-        return False
-
+        for crv in self.crvs:
+            if self._is_intersects(crv, ne_bb):
+                ne.insert(crv)
+            if self._is_intersects(crv, nw_bb):
+                nw.insert(crv)
+            if self._is_intersects(crv, se_bb):
+                se.insert(crv)
+            if self._is_intersects(crv, sw_bb):
+                sw.insert(crv)
+    
+    def _is_intersects(self, crv, bb):
+        intersects = rg.Intersect.Intersection.CurveCurve(
+            crv, bb, ConstsCollection.TOLERANCE, ConstsCollection.TOLERANCE
+        )
+        
+        return len(intersects) > 0
+    
     def insert(self, crv):
         if self.children:
             for child in self.children:
-                if self._is_intersects(crv):
+                
+                if self._is_intersects(crv, self.bounding_box):
                     child.insert(crv)
         else:
             self.crvs.append(crv)
+            
+            if (
+                len(self.crvs) >= self.COUNTER
+                and (self.width > self.min_size or self.height > self.min_size)
+            ):
+                self._subdivide()
+                
+def get_all_quadtree_geometries(quadtree):
+    all_quadtree_geometries = []
+    
+    if quadtree.children:
+        for child in quadtree.children:
+            geoms = get_all_quadtree_geometries(child)
+            
+            if geoms is not None:
+                all_quadtree_geometries.extend(geoms)
+    
+    elif quadtree.children is None:
+        all_quadtree_geometries.append(quadtree.bounding_box)
+    
+    else:
+        for child_geometry in quadtree.children_geometries:
+            all_quadtree_geometries.append(child_geometry)
+    
+    return all_quadtree_geometries
 
-
-#            if (
-#                len(self.crvs) > self.COUNTER
-#                and self.width > self.MIN_SIZE
-#                and self.height > self.MIN_SIZE
-#            ):
-#                self.subdivide()
 
 if __name__ == "__main__":
     plane = rg.Plane.WorldXY
-
-    l = rg.PolylineCurve([rg.Point3d(0, 0, 0), rg.Point3d(1, 1, 1)])
+    
+    l = rg.PolylineCurve([rg.Point3d(0,0,0), rg.Point3d(1,3,0)])
     bounding_box = LineHelper.get_2d_obb_from_line(
-        #        rg.PolylineCurve([plane.Origin, plane.Origin + plane.XAxis]),
+#        rg.PolylineCurve([plane.Origin, plane.Origin + plane.XAxis]),
         l,
-        input_curve,
+        input_curve
     ).ToNurbsCurve()
-
-    quadtree = Quadtree(10, bounding_box)
-    a = bounding_box
-
+    
+    quadtree = Quadtree(min_size=5, bounding_box=bounding_box)
     exploded_curve = input_curve.DuplicateSegments()
-    for c in exploded_curve:
-        quadtree.insert(c)
+    for crv in exploded_curve:
+        quadtree.insert(crv)
+        
+    quadtree_geometries = get_all_quadtree_geometries(quadtree)
