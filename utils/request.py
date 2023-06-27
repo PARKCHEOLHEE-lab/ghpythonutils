63c613a6d99d6d6365bb5507067b16886e90110a
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-06-27 19:43:48 +0900

    request.py

---
+++ utils/request.py
@@ -0,0 +1,33 @@
+﻿import json
+
+import urllib2
+
+
+class Request:
+    def __init__(self, common_url):
+        self.common_url = common_url
+
+    def _request_get(self, url, data):
+        """GET request method for flask API"""
+
+        return
+
+    def _request_post(self, url, data):
+        """POST request method for flask API"""
+
+        request = urllib2.Request(
+            url=url,
+            data=json.dumps(data),
+            headers={"Content-Type": "application/json"},
+        )
+
+        try:
+            response = urllib2.urlopen(request)
+            response_data = response.read()
+
+        except urllib2.HTTPError as e:
+            print("HTTP Error:", e.code, e.reason)
+            response_data = e.read()
+            print(response_data)
+
+        return json.loads(response_data)
